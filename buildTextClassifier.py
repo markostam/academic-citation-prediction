5813e1ada65efa177365a004abe29beca7e08496
Sam Schick <schicksw@gmail.com>
2016-01-29 16:01:23 -0500

    Added untested text classifier creation.

---
+++ buildTextClassifier.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python2
+
+import sys
+import numpy as np
+import os
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.externals import joblib
+
+#to use; python2 buildTextClassifier /path/to/text
+
+folder=sys.argv[1]
+files=os.listdir(folder)
+classes=list()
+pattern=re.compile(r"([0-9]+)-")
+for f in files:
+	cites=pattern.find(f)
+	if cites:
+		if int(cites.group(1))>10: classes.append(True) else: classes.append(False)
+	else: 
+		print("WARNING: file name not formatted correctly. giving up.")
+		break
+
+	f=folder+"/"+f
+classes=np.array(classes)
+extract=TfidfVectorizer(input='filename', stop_words='english')
+data=extract.fit_transform(files)
+classify=LinearSVC()
+classify.fit(data,classes)
+attributes=extract.get_feature_names()
+joblib.dump((classify, files, data, attributes),"text.pkl", compress=3)
