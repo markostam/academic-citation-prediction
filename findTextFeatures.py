72fe446d7c3060dd701a7379ed71ce2ebb9905c2
markostam <markostam@gmail.com>
2016-03-20 22:52:37 -0400

    Revert "reorganizing project folder, getting rid of clutter, rename main fxn"
    This reverts commit d50ab7f632f52822b7dcd6e08a2415f7fcce994d.

---
+++ findTextFeatures.py
@@ -0,0 +1,48 @@
+#!/usr/local/bin/python2
+
+import sys
+import numpy as np
+import os
+import re
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.externals import joblib
+
+#to use; python2 buildTextClassifier /path/to/text
+
+folder=sys.argv[1]
+names=os.listdir(folder)
+files=list()
+classes=list()
+print(os.getcwd())
+#regex to find class
+pattern=re.compile(r"([0-9]+)-")
+for f in names:
+	cites=pattern.search(f)
+	if cites:
+		if int(cites.group(1))>10: 
+			classes.append(True) 
+		else: 
+			classes.append(False)
+	else: 
+		print("WARNING: file name not formatted correctly.")
+		print(f)
+		answer=input("would you like to ignore this file? y/N")
+		if answer=="y":
+			pass
+		else:
+			print("giving up")
+			break
+
+	files.append(os.path.join(folder,f))
+	print(f)
+classes=np.array(classes)
+#feature extraction
+extract=TfidfVectorizer(input='filename', stop_words='english')
+train_data=extract.fit_transform(files)
+vocab=extract.get_feature_names()
+#train classifier
+classify=LinearSVC()
+classify.fit(train_data,classes)
+#serialize
+joblib.dump((txt_clf, vocab),os.path.join(os.getcwd(),"text.pkl"), compress=3)
